9cac98f66001b34d1c086fe9a895dfa41dfa9b41
Darien O. Davis <dariendarien03@gmail.com>
2022-02-07 19:42:43 -0500

    Update script

--- script_dev.py
+++ script_dev.py
@@ -1,4 +1,8 @@
+# Assigning x to a equation that outputs an integer
 x=(int(2+2))
+# printing the variable x
 print (x)
-x=(int(x+4))
+# reassinging x to a new equation that outputs a float 
+x=(float(x+4))
+# printing the variable x
 print (x)
